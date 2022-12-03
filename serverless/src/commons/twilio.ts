d5b62c041230f47dfea836762060968bed9f7adc
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-12-03 23:20:26 +0900

    twilioにて電話をかける処理は外に弾き出した

---
+++ serverless/src/commons/twilio.ts
@@ -0,0 +1,28 @@
+import twilio from 'twilio';
+
+const tilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+export async function twilioCreateCall({
+  twimlString,
+  toPhoneNumber,
+  statusCallbackUrl = null,
+}: {
+  twimlString: string;
+  toPhoneNumber: string;
+  statusCallbackUrl?: string;
+}): Promise<any> {
+  const createCallObj = {
+    twiml: twimlString,
+    from: process.env.TWILIO_US_PHONE_NUMBER,
+    to: toPhoneNumber,
+  };
+  if (statusCallbackUrl) {
+    return tilioClient.calls.create({
+      ...createCallObj,
+      statusCallback: statusCallbackUrl,
+      statusCallbackMethod: 'POST',
+    });
+  } else {
+    return tilioClient.calls.create(createCallObj);
+  }
+}
