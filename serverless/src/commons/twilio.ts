03782ecd7829b9dd837981374433be6a70cb7495
daiking1756 <daiking1756@gmail.com>
2022-12-04 20:33:52 +0900

    Fix typo(tilio -> twilio)

--- serverless/src/commons/twilio.ts
+++ serverless/src/commons/twilio.ts
@@ -1,6 +1,6 @@
 import twilio from 'twilio';
 
-const tilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export async function twilioCreateCall({
   twimlString,
@@ -17,18 +17,18 @@ export async function twilioCreateCall({
     to: toPhoneNumber,
   };
   if (statusCallbackUrl) {
-    return tilioClient.calls.create({
+    return twilioClient.calls.create({
       ...createCallObj,
       statusCallback: statusCallbackUrl,
       statusCallbackMethod: 'POST',
     });
   } else {
-    return tilioClient.calls.create(createCallObj);
+    return twilioClient.calls.create(createCallObj);
   }
 }
 
 export async function twilioSendSMS({ message, toPhoneNumber }: { message: string; toPhoneNumber: string }): Promise<any> {
-  return tilioClient.messages.create({
+  return twilioClient.messages.create({
     body: message,
     from: process.env.TWILIO_US_PHONE_NUMBER,
     to: toPhoneNumber,
