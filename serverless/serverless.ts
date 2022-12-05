23574b8011b2e5f4cc7fc694bf7b2cf2d6564537
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-12-05 13:49:45 +0900

    targetをnode16にした

--- serverless/serverless.ts
+++ serverless/serverless.ts
@@ -51,7 +51,7 @@ const serverlessConfiguration: AWS = {
       minify: false,
       sourcemap: true,
       exclude: ['aws-sdk'],
-      target: 'node14',
+      target: 'node16',
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
